1c775628a460ac2b8854f9cd8ffc20df5893a4f1
Eugenio De Simone <eugenio12simone@gmail.com>
2023-06-06 17:50:21 +0200

    Working on . . .

--- main.py
+++ main.py
@@ -28,28 +28,14 @@ def count_degree(edges_labeled):
     return v_dict,n_dict
 
 
-def get_Neighbours_ofID(edges_labeled,v_dict,nodeID, sign_value):
-    #this function provides a list of all the nodes that have an edgeOut in nodeID
-    neighbours = []
+def graph_to_dict(graph,v_dict,n_dict,t):
 
-    for e in edges_labeled:
-        if(e[1] == nodeID and e[2] == sign_value and e[0] in v_dict):
-            neighbours.append(e[0])
+    graph_dict = {}
+    for node in graph.Nodes():
+        graph_dict[node.GetId()] = (v_dict[node.GetId()],n_dict[node.GetId()],t)
 
-    return neighbours
-
-def compact_dict(v_dict,n_dict):
-    #[[u,d+,d-]...]  <---> dict[key:u] = (d+,d-)
-    comp_dict = {}
-    for v in v_dict.keys():
-        comp_dict[v] = (v,v_dict[v],0)
-    for v in n_dict.keys():
-        if v in comp_dict:
-            tupla = comp_dict[v]
-            comp_dict[v] = (v,tupla[0],n_dict[v])
-        else:
-            comp_dict[v] = (v, 0 ,n_dict[v])
-    return comp_dict
+    return graph_dict
+# graph_dict[key:nodo] = (d+,d-,t)
 
 def labeling(graph):
     min_probability = 0  # Valore minimo di probabilità
@@ -58,6 +44,7 @@ def labeling(graph):
     random.seed(time.time())
 
     edges = []
+
     for edge in graph.Edges():
 
         u = edge.GetSrcNId()
@@ -68,48 +55,13 @@ def labeling(graph):
         normalized_probability = (probability - min_probability) / (max_probability - min_probability)
 
         random_value = random.uniform(0,1)
-        #print("u",u,"v",v,"RAND",random_value,"NORM_P",normalized_probability)
+
         if random_value <= normalized_probability:
-            edges.append((u, v, -1))  # Arco negativo
+            edges.append((u,v,-1))  # Arco negativo
         else:
-            edges.append((u, v, 1))  # Arco positivo
+            edges.append((u,v,1))  # Arco positivo
 
     return edges
-
-def labeling_graph(graph, t):
-    min_probability = 0  # Valore minimo di probabilità
-    max_probability = 1  # Valore massimo di probabilità
-
-    random.seed(time.time())
-
-    edges = []
-    for edge in graph.Edges():
-        u = edge.GetSrcNId()
-        v = edge.GetDstNId()
-
-        #Ciclo da eliminare
-        if u == v:
-            graph.DelEdge(u, v)
-            break
-
-        max_degree = max(graph.GetNI(u).GetDeg(), graph.GetNI(v).GetDeg())
-
-        probability = 1 / (max_degree + 1)
-        normalized_probability = (probability - min_probability) / (max_probability - min_probability)
-
-        random_value = random.uniform(0,1)
-        #print("u",u,"v",v,"RAND",random_value,"NORM_P",normalized_probability)
-        if random_value <= normalized_probability:
-            graph.AddIntAttrDatE(edge, -1, "weight")
-        else:
-            graph.AddIntAttrDatE(edge, 1, "weight")
-
-    return graph
-
-def set_t(graph,t):
-
-    for node in graph.Nodes():
-        graph.AddIntAttrDatN(node,t,"t")
 
 
 def first_algorithm(edges_labeled,k):
@@ -127,6 +79,7 @@ def first_algorithm(edges_labeled,k):
     return seed_set
 
 
+'''
 def second_algorithm(edges_labeled,k):
     # second algorithm Seeds Greedy Residual Degree Max
 
@@ -146,106 +99,48 @@ def second_algorithm(edges_labeled,k):
                 v_dict[w] = v_dict[w] - 1
 
     return seed_set
-
-'''
-def third_algorithm(edges_labeled,k,t):
-    seed_set = set()
-
-    #v_dict = + DEGREE   - n_dict = - DEGREE
-    v_dict, n_dict = count_degree(edges_labeled)
-    comp_dict = compact_dict(v_dict,n_dict)
-
-    diff = 0
-    while len(seed_set) < k:
-        difference_dict = {}
-        for e in edges_labeled: #e(u,v,peso)   comp[key:u] = (d+,d-)
-            if e[0] not in comp_dict: break
-            tupla = comp_dict[e[0]]
-            diff = tupla[0] - tupla[1]
-
-            if diff >= 0:
-                tot = diff / t
-                difference_dict[e[0]] = tot
-
-        if len(difference_dict) == 0:
-            return "ERROR DIFF EMPTY"
-
-        max_ID = max(difference_dict, key=difference_dict.get)
-        seed_set.add(max_ID) # Add the new v in the seed set
-        del comp_dict[max_ID]
-
-        for e in edges_labeled:
-
-            if e[0] not in comp_dict: break
-            tupla = comp_dict[e[0]]
-
-            if e[1] == max_ID:
-                if e[2] == 1:
-                    comp_dict[e[0]] = (tupla[0] - 1,tupla[1])
-                else:
-                    comp_dict[e[0]] = (tupla[0] , tupla[1] - 1)
-        #w vicini di maxID che hanno archi positivi verso maxID
-        #z vicini di maxID he hanno archi negativi verso maxID
-        # we have to reduce the degree of the neighbour of max_ID
-
-    return seed_set
 '''
 
-
-def third_algorithm(graph, k, t):
+def third_algorithm(graph, k, graph_dict):
     seed_set = set()
-    # Inizializza il Counter per il conteggio degli archi positivi e negativi
-    counter = Counter()
 
-    for edge in graph.Edges():
-        src_node_id = edge.GetSrcNId()
-        weight = graph.GetIntAttrDatE(edge, "weight")
-
-        if weight > 0:
-            # Verifica se il nodo di origine è già presente nel contatore
-            if src_node_id in counter:
-                # Incrementa il valore 'positive' del contatore del nodo di origine
-                counter[src_node_id]['positive'] += 1
-            else:
-                # Se il nodo di origine non è presente, crea una nuova voce nel contatore
-                counter[src_node_id] = {'positive': 1, 'negative': 0}
-        elif weight < 0:
-            # Verifica se il nodo di origine è già presente nel contatore
-            if src_node_id in counter:
-                # Incrementa il valore 'positive' del contatore del nodo di origine
-                counter[src_node_id]['negative'] += 1
-            else:
-                # Se il nodo di origine non è presente, crea una nuova voce nel contatore
-                counter[src_node_id] = {'positive': 0, 'negative': 1}
-
+    dict = copy.deepcopy(graph_dict)# graph_dict[key:nodo] = (d+,d-,t)
 
     while len(seed_set) < k:
-        # Calcola il massimo valore (grado positivo - grado negativo) / t nel Counter se il grado positivo è maggiore del grado negativo
-        max_node_id = max(
-            (x for x, v in counter.items() if v['positive'] > v['negative']),
-            key=lambda x: (counter[x]['positive'] - counter[x]['negative']) / t)
-        # Rimuovi l'elemento dal Counter
-        counter.pop(max_node_id)
-        seed_set.add(max_node_id)
+        calculate_P = lambda node: (dict[node][0] - dict[node][1]) / dict[node][2] if dict[node][0] > dict[node][1] else float('-inf')
 
-        # Recupera tutti i nodi con max_node_id come vicino
-        neighbours = [node.GetId() for node in graph.Nodes() if graph.IsEdge(node.GetId(), max_node_id)]
+        # Trova il nodeID che massimizza P
+        max_node = max(dict, key=calculate_P)
+        print(max_node)
 
-        # Rimuovi archi positivi o negativi dai vicini
-        for neighbour in neighbours:
-            edge = graph.GetEI(neighbour, max_node_id)
-            weight = graph.GetIntAttrDatE(edge, "weight")
 
-            # Controllo se l'arco è positivo o negativo
-            if weight > 0:
-                counter[neighbour]['positive'] -= 1
-            else:
-                counter[neighbour]['negative'] -= 1
-        graph.DelNode(max_node_id)  # Rimuovi il nodo dal grafo
+
 
     return seed_set
 
+def fourth_alhorithm(graph,k,t):
+    #comunity -> utilizzare
+    print("d")
 
+def comunities_detection(graph):
+
+    # Converti il grafo TNEANet in un grafo PUNGraph (ignorando gli attributi sugli archi)
+    G = snap.ConvertGraph(snap.PUNGraph, graph)
+
+    # Esegui l'algoritmo di community detection
+    CmtyV = snap.TCnComV()
+    modularity = snap.CommunityGirvanNewman(G, CmtyV)
+
+    # Stampa le informazioni sulle community
+    print("Numero di community individuate:", len(CmtyV))
+    print("Modularità:", modularity)
+
+    # Stampa i nodi appartenenti a ciascuna community
+    for i, Cmty in enumerate(CmtyV):
+        print("Community", i + 1, ":", )
+        for NI in Cmty:
+            print(NI, end=" ")
+        print()
 
 
 def TSS_algorithm(graph,k):
@@ -268,6 +163,8 @@ def TSS_algorithm(graph,k):
                     graph.AddIntAttrDatN(u, graph.GetIntAttrDatN(u, 't') - 1, 't')
             vertices.remove(v_with_t_zero)
         else:
+            max_u = None
+            max_value = -1
             v_with_d_less_t = None
             for v in vertices:
                 if graph.GetNI(v).GetDeg() < graph.GetIntAttrDatN(v, 't'):
@@ -284,20 +181,19 @@ def TSS_algorithm(graph,k):
                         graph.AddIntAttrDatN(u, graph.GetIntAttrDatN(u, 't') - 1, 't')
                 vertices.remove(v_with_d_less_t)
             else:
-                max_u = None
-                max_value = -1
-
                 for v in vertices:
                     d = graph.GetNI(v).GetDeg()
                     value = graph.GetIntAttrDatN(v, 't')/( d * (d + 1) )
                     if value > max_value:
                         max_value = value
+                        print("value", value, "max_value", max_value, "v", v, "max_u",max_u)
                         max_u = v
 
                 for edge in graph.Edges():
                     u = edge.GetDstNId()
-                    if u == graph.GetNI(v_with_d_less_t):
-                        graph.DelEdge(u, max_u)
+                    src = edge.GetSrcNId()
+                    if u == max_u:
+                        graph.DelEdge(src, max_u)
                 vertices.remove(max_u)
 
     return seed_set
@@ -308,25 +204,25 @@ def cascade(graph,seed_set):
     influencing = copy.deepcopy(seed_set)
 
     while len(influencing) != len(prev_influenced):
-        nodes_plus = 0
-        nodes_minus = 0
         prev_influenced = copy.deepcopy(influencing)
         for node in graph.Nodes():
+            nodes_plus = 0
+            nodes_minus = 0
+            if node.GetId() in prev_influenced:
+                continue
             if node.GetId() not in prev_influenced:
                 for edge in graph.Edges():
                     u = edge.GetSrcNId()
                     v = edge.GetDstNId()
                     if u == node.GetId() and v in prev_influenced:
-                        if graph.GetIntAttrDatN(graph.GetNI(u), "weight") == 1:
+                        if graph.GetIntAttrDatE(edge, "weight") == 1:
                             nodes_plus += 1
                         else:
                             nodes_minus += 1
+                        print("nodo:", node.GetId(), "plus:", nodes_plus,"minus",nodes_minus)
                 if (nodes_plus - nodes_minus) >= graph.GetIntAttrDatN(node,"t"):
                     influencing.add(node.GetId())
     return influencing
-
-
-
 
 
 def printGr(filename):
@@ -344,7 +240,7 @@ def printGr(filename):
     plt.show()
 
 if __name__ == '__main__':
-    filename = "datasets/twitter_combined.txt"
+    filename = "datasets/p2p-Gnutella08.txt"
 
     parser = argparse.ArgumentParser()
 
@@ -356,12 +252,12 @@ if __name__ == '__main__':
 
 
     # Load the Graph from Edge List file
-    graph = snap.LoadEdgeList(snap.TNEANet,filename , 0, 1)
+    graph = snap.LoadEdgeList(snap.TUNGraph,filename , 0, 1)
 
     '''
     random.seed(42)
     graph = snap.TNEANet.New()
-    for i in range(1, 11):
+    for i in range(1, 50):
         graph.AddNode(i)
 
     for u in graph.Nodes():
@@ -373,28 +269,31 @@ if __name__ == '__main__':
                     graph.AddEdge(u.GetId(), v.GetId())
     print("Numero di nodi:", graph.GetNodes())
     print("Numero di archi:", graph.GetEdges())
-    '''
+    
     
     #for edge in graph.Edges():
      #   print(edge.GetSrcNId(), "->", edge.GetDstNId())
     #for node in graph.Nodes():
      #   print(node.GetId())
+    '''
 
     #Labeling
-    labeling_graph(graph,args.t)
-    set_t(graph,args.t)
+    edges_labeled = labeling(graph)
+
+    v_dict,n_dict = count_degree(edges_labeled)
+    print("v",v_dict,"n",n_dict)
+    graph_dict = graph_to_dict(graph,v_dict,n_dict,args.t)
+
+    third_algorithm(graph,args.k,graph_dict)
 
 
+
+
+'''
     file_path = "graph.bin"
     FOut = snap.TFOut(file_path)
     graph.Save(FOut)
     FOut.Flush()
     FIn = snap.TFIn(file_path)
     loaded_graph = snap.TNEANet.Load(FIn)
-
-
-    seed_set = TSS_algorithm(loaded_graph,args.k)
-    print(seed_set)
-
-    #influenced = cascade(graph, seed_set)
-    #print(influenced)
+    '''
